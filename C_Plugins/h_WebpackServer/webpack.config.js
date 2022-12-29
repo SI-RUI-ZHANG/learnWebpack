e0398e29aedf5a5b73b62b3e63bf1d3d9a853327
Sirui Zhang <siruizhangdev@gmail.com>
2022-12-29 13:07:46 -0500

    change folder structure

---
+++ C_Plugins/h_WebpackServer/webpack.config.js
@@ -0,0 +1,38 @@
+import path from 'path';
+import {fileURLToPath} from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+const config = {
+  entry: {
+    index: "./src/index.js",
+    product: "./src/products.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true
+  },
+  devServer: {
+static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html"),
+      chunks: ["index"],
+      inject: true,
+      filename: "index.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "products.html"),
+      chunks: ["product"],
+      inject: true,
+      filename: "products.html",
+    })
+  ]
+};
+
+export default config;
