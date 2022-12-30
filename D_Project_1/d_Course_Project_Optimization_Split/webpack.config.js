c41a5cfe16c3cbccabbb8f01b58cd81f12126baa
Sirui Zhang <siruizhangdev@gmail.com>
2022-12-30 07:44:50 -0500

    work on split code

---
+++ D_Project_1/d_Course_Project_Optimization_Split/webpack.config.js
@@ -0,0 +1,62 @@
+import path from "path";
+import {fileURLToPath} from "url";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import CopyWebpackPlugin from "copy-webpack-plugin";
+import {BundleAnalyzerPlugin} from "webpack-bundle-analyzer";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const config = {
+  entry: {index: "./src/index.js", courses: "./src/pages/courses.js",},
+  output: {filename: "[name].bundle.js", path: path.resolve(__dirname, "dist"), clean: true,},
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html"),
+      chunks: ["index"],
+      inject: true,
+      filename: "index.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "pages", "courses.html"),
+      chunks: ["courses"],
+      inject: true,
+      filename: "courses.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/assets/images/*"),
+          to: path.resolve(__dirname, "dist"),
+          context: "src",
+        }
+      ]
+    }),
+    new BundleAnalyzerPlugin({})
+  ],
+  devServer: {
+    static: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        type: "asset/resource",
+      }
+    ]
+  }
+};
+
+export default config;
